094aa11e9bfd3a6ea78fc4d1ce18512c1ad555ca
Uno Zero Code <57536442+unozerocode@users.noreply.github.com>
2019-11-12 21:58:32 -0600

    Create ex03.py
    closes #03

---
+++ code/ex03.py
@@ -0,0 +1,32 @@
+class Solution: 
+    def longestPalindrome(self, s):
+        longest = ""
+        substrings = Solution().substrings(s)
+        for sub in substrings:
+          revsub = sub[::-1]
+          revindex = s.find(revsub)
+          if revindex > 0 :
+              if len(revsub) > len(longest):
+                longest = revsub
+        return longest
+    
+    def substrings(self,s):
+      r = []
+      for i in range(0, len(s)):
+        for j in range(i+2,len(s)+1):
+          #print(f"{i},{j} {s[i:j:1]}")
+          r.append(f"{s[i:j:1]}")
+      return r
+          
+    def reverse(self, s):
+      return s[::-1]
+# Test program
+
+print(Solution().longestPalindrome("tracecars"))
+# racecar
+
+print(Solution().longestPalindrome("banana"))
+#Output: "anana"
+
+print(Solution().longestPalindrome("million"))
+#Output: "illi"
